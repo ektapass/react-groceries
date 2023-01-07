6709561a15e7ddf825638e773546a1c88de5d700
Ekta Bharti <ektabharti019@gmail.com>
2023-01-06 19:44:17 -0500

    first commit

---
+++ src/groceries.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      item:'Apple', 
+      brand:'Fuji' ,
+      units: 'lb',
+      quantity:8,
+      isPurchased:false
+    },
+    {
+        item:'Orange juice', 
+        brand:'Tropicana' ,
+        units: 'lt',
+        quantity:2,
+        isPurchased:false
+      },
+      {
+        item:'Bread', 
+        brand:'Oroweat' ,
+        units: '16 oz',
+        quantity:5,
+        isPurchased:false
+      }
+]
